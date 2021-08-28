61dfad92a9e6d459d22af005294443e990fc5b92
GadiegoN <ngadiego@gmail.com>
2021-08-28 19:36:45 -0300

    :package: Preview do post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const { slug } = params;
 
     // console.log(session)
-    if(!session.activeSubscription) {
+    if(!session?.activeSubscription) {
         return {
             redirect: {
                 destination:'/',
